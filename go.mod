aa454b88d94493713680e35b7cf473f0a9e537e5
hulk510 <dorian.51069@gmail.com>
2021-11-07 16:22:37 +0900

    とりあえず一旦できたのでコミット

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hulk510/switch-gh
 
 go 1.16
+
+require github.com/joho/godotenv v1.4.0
--- main.go
+++ main.go
@@ -1,3 +1,56 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
-	
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// ユーザーを取得
+	tokenFlag := flag.String("u", "private", "変えたいユーザー")
+	flag.Parse()
+	var token string
+	switch *tokenFlag {
+	case "private":
+		token = os.Getenv("PRIVATE_ACCESS_TOKEN")
+	case "enterprise":
+		token = os.Getenv("ENTERPRISE_ACCESS_TOKEN")
+	}
+
+	cmd := exec.Command("gh", "auth", "login", "--with-token")
+
+	// このpipeで何してるのか謎出力をwith-tokenに渡せてるっぽいので成功するんやけどここら辺イミフ。
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		panic(err)
+	}
+
+	io.WriteString(stdin, token)
+	stdin.Close()
+
+	// 結果を取得するのとエラーを標準エラーで表示してくれるように書き込み先を変更
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+
+	err = cmd.Run()
+	if err != nil {
+		fmt.Println(stderr.String())
+		panic(err)
+	}
+
+	fmt.Println("Successfully change user!!")
 }
